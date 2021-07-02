d5aa01364b096821f34a086ad4629698c5c50e34
bigden <396134735@qq.com>
2021-07-02 15:08:28 +0800

    change dir

---
+++ learn_vue3/010-组件化开发/component-a.vue
@@ -0,0 +1,25 @@
+<template id="com1">
+  <div>
+    <div>{{title}}!!</div>
+    <button @click="ccc">点击</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'item'
+    }
+  },
+  methods: {
+    ccc() {
+      alert('aaaaaaaa')
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
