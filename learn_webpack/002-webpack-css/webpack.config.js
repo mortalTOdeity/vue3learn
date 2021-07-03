3a991d32bf5e278a79e13bf1a584335160886ced
chen <396134735@qq.com>
2021-07-03 18:27:40 +0800

    learn

--- learn_webpack/002-webpack-css/webpack.config.js
+++ learn_webpack/002-webpack-css/webpack.config.js
@@ -4,6 +4,39 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'main.js'
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // 下面是语法糖
+        // loader: "css-loader"
+        use: [
+          // use 的顺序是从下往上执行
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'less-loader',
+          },
+        ]
+      },
+    ]
   }
 }
