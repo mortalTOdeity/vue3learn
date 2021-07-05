ff997480f0c4a20863c65845b605e64776a785ae
bigden <396134735@qq.com>
2021-07-05 17:20:31 +0800

    learn

---
+++ learn_webpack/004-webpack-插件/src/index.js
@@ -0,0 +1,6 @@
+import { sum } from './js/math.js'
+const { priceFromat } = require('./js/format.js')
+import './js/element'
+
+console.log(sum(10, 20))
+console.log(priceFromat())
---
+++ learn_webpack/004-webpack-插件/src/js/format.js
@@ -0,0 +1,8 @@
+// CommonJs 的 导出
+const priceFromat = function() {
+  return "$99.88"
+}
+
+module.exports = {
+  priceFromat
+}
