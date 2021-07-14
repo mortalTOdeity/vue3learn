cb943449b60c4fae7a45c6dd1b6c26dfc6b41a67
chen <396134735@qq.com>
2021-07-15 00:13:26 +0800

    add

---
+++ learn_component/src/06_Provide和Inject使用/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './Home.vue'
+  export default {
+  components: { Home },
+  provide: {
+    name: 'chen',
+    age: 18
+  }  
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ learn_component/src/06_Provide和Inject使用/HomeContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    HomeContent-{{name}}---{{age}}
+  </div>
+</template>
+
+<script>
+  export default {
+    inject: ['name', 'age']
+  }
+</script>
+
+<style scoped>
+
+</style>
