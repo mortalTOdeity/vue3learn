fa116281fd46fd3abb1f7d13ba419abd8b2d3ee0
chen <396134735@qq.com>
2021-07-15 23:38:28 +0800

    add

---
+++ learn_animation/src/01_动画的基本使用/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="toggle">切换</button>
+    <transition name="chen">
+      <h2 v-if="isShow">Hello World</h2>
+    </transition>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isShow: true
+      }
+    },
+    methods: {
+      toggle() {
+        this.isShow = !this.isShow
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .chen-enter-from,
+  .chen-leave-fo {
+    opacity: 0;
+  }
+</style>
