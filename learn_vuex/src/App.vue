1c83e149e1bfd2ea00daef9a73ee99bc6d17add7
bigden <396134735@qq.com>
2021-07-28 16:05:02 +0800

    dd

--- learn_vuex/src/App.vue
+++ learn_vuex/src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>{{$store.state.counter}}</h2>
+  <button @click="increment">+1</button>
+  <button @click="decrement">-1</button>
+  <home></home>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './views/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   },
-  data() {
-    return {
-      message: 'hhhh'
-    }
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    },
+    decrement() {
+      this.$store.commit('decrement')
+    },
   },
 }
 </script>
